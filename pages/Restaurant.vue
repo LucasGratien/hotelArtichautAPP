8a2b636087c29b7eb8e59a3d34b97892769b0f62
Lucas Gratien <lucas.gratien@le-campus-numerique.fr>
2025-01-15 17:08:05 +0100

    probleme

--- pages/Restaurant.vue
+++ pages/Restaurant.vue
@@ -1,6 +1,18 @@
 <template>
- <div> <UiHerobanner/>
+ <div>
+   <div>
+     <UiHerobanner :title="heroPageData?.title"
+                   :text="heroPageData?.text"
+                   :button="heroPageData?.button"
+                   :image="heroPageData?.image" />
+   </div>
  </div>
 </template>
 <script setup lang="js">
+const heroPageData =  {
+  title: "Restaurant",
+      texte: "Savourez l'excellence, où chaque moment devient une expérience",
+      button: "Booking",
+      image: "/assets/image/restaurant-hall-with-round-square-tables-some-chairs-plants.png",
+}
 </script>
