0e0d82532d58fb69b3c179773251ec338ad0d6e9
Lucas Gratien <lucas.gratien@le-campus-numerique.fr>
2025-01-17 16:35:43 +0100

    cardpages

--- pages/Rooms.vue
+++ pages/Rooms.vue
@@ -1,5 +1,5 @@
 <template>
-  <UiHerobanner :title="heroPageData?.title"
+<div>  <UiHerobanner :title="heroPageData?.title"
                 :text="heroPageData?.text"
                 :button="heroPageData?.button"
                 :image="heroPageData?.image" />
@@ -11,6 +11,23 @@
       link="/reservation"
       buttonText="Réserver"
   />
+  <UiCardpages
+      title="Luxury rooms"
+      description="La chambre standard de notre hôtel allie confort et simplicité pour un séjour agréable. Elle est équipée d'un lit double spacieux, d'une télévision à écran plat, d'une connexion Wi-Fi gratuite, et d'une salle de bain privative avec douche ou baignoire. Un bureau fonctionnel et un espace de rangement sont également à votre disposition. La décoration moderne et chaleureuse offre une ambiance propice à la détente. Idéale pour les séjours professionnels ou touristiques, cette chambre vous garantit un repos optimal."
+      image="/assets/image/parisluxe.png"
+      imageAlt="Chambre de luxe"
+      link="/reservation"
+      buttonText="Réserver"
+      :inverted="true"
+  />
+  <UiCardpages
+      title="Luxury rooms"
+      description="La chambre standard de notre hôtel allie confort et simplicité pour un séjour agréable. Elle est équipée d'un lit double spacieux, d'une télévision à écran plat, d'une connexion Wi-Fi gratuite, et d'une salle de bain privative avec douche ou baignoire. Un bureau fonctionnel et un espace de rangement sont également à votre disposition. La décoration moderne et chaleureuse offre une ambiance propice à la détente. Idéale pour les séjours professionnels ou touristiques, cette chambre vous garantit un repos optimal."
+      image="/assets/image/parisluxe.png"
+      imageAlt="Chambre de luxe"
+      link="/reservation"
+      buttonText="Réserver"
+  /></div>
 </template>
 <script setup lang="ts">
 const heroPageData = {
