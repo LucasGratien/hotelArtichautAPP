76968b37214fa8eb4c729b14f355a28cacd6d26e
RDelory74 <roro_spike@hotmail.com>
2025-01-20 14:57:52 +0100

    update size title Herobanner.vue Theheader.vue

--- components/layout/TheHeader.vue
+++ components/layout/TheHeader.vue
@@ -4,7 +4,7 @@
 
 
       <div class="flex items-center space-x-2">
-        <img class="h-16" src="public/assets/image/logoArtichaut.png" alt="Logo" />
+        <img class="h-10 md:h-16" src="public/assets/image/logoArtichaut.png" alt="Logo" />
         <div class="title-wrapper flex flex-col items-start ">
           <UiTittle title="L'Artichaut"></UiTittle>
           <p class="text-center w-full -mt-3">* * *</p>
@@ -33,7 +33,5 @@
 .title-wrapper p {
  color: var(--primary-color);
 }
-img {
-  @apply h-16;
-}
+
 </style>
