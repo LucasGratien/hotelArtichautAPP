a01db94363f7047fe9cd5aa7f43777b197d4a6a5
RDelory74 <roro_spike@hotmail.com>
2025-01-20 14:21:19 +0100

    update mx ReviewCard.vue Review.vue

--- components/ui/Review.vue
+++ components/ui/Review.vue
@@ -4,7 +4,7 @@
     <div class="flex justify-center items-center">
       <img src="public/assets/image/component_separator.png" alt="separator">
     </div>
-    <div class="relative w-full max-w-8xl mx-auto px-0 md:px-8">
+    <div class="relative w-full max-w-8xl mx-auto md:px-8">
       <UCarousel
           :items="reviews"
           :ui="{
@@ -40,6 +40,7 @@
 </template>
 
 <script setup lang="ts">
+
 interface Review {
   id: number;
   rate: number;
@@ -50,7 +51,6 @@ interface Review {
 const { data: reviews } = await useFetch<Review[]>('/review/', {
   baseURL: useRuntimeConfig().public.apiBase,
   default: () => []
-
 })
 
 // const reviews = [
