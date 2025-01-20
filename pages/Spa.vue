b7a43996b54149628aace96947cc4c401b626518
Lucas Gratien <lucas.gratien@le-campus-numerique.fr>
2025-01-20 14:19:52 +0100

    police banner

--- pages/Spa.vue
+++ pages/Spa.vue
@@ -3,6 +3,31 @@
                 :text="heroPageData?.text"
                 :button="heroPageData?.button"
                 :image="heroPageData?.image" />
+  <UiCardpages
+      title="Luxury rooms"
+      description="La chambre standard de notre hôtel allie confort et simplicité pour un séjour agréable. Elle est équipée d'un lit double spacieux, d'une télévision à écran plat, d'une connexion Wi-Fi gratuite, et d'une salle de bain privative avec douche ou baignoire. Un bureau fonctionnel et un espace de rangement sont également à votre disposition. La décoration moderne et chaleureuse offre une ambiance propice à la détente. Idéale pour les séjours professionnels ou touristiques, cette chambre vous garantit un repos optimal."
+      image="/assets/image/parisluxe.png"
+      imageAlt="Chambre de luxe"
+      link="/reservation"
+      buttonText="Réserver"
+  />
+  <UiCardpages
+      title="Luxury rooms"
+      description="La chambre standard de notre hôtel allie confort et simplicité pour un séjour agréable. Elle est équipée d'un lit double spacieux, d'une télévision à écran plat, d'une connexion Wi-Fi gratuite, et d'une salle de bain privative avec douche ou baignoire. Un bureau fonctionnel et un espace de rangement sont également à votre disposition. La décoration moderne et chaleureuse offre une ambiance propice à la détente. Idéale pour les séjours professionnels ou touristiques, cette chambre vous garantit un repos optimal."
+      image="/assets/image/parisluxe.png"
+      imageAlt="Chambre de luxe"
+      link="/reservation"
+      buttonText="Réserver"
+      :inverted="true"
+  />
+  <UiCardpages
+      title="Luxury rooms"
+      description="La chambre standard de notre hôtel allie confort et simplicité pour un séjour agréable. Elle est équipée d'un lit double spacieux, d'une télévision à écran plat, d'une connexion Wi-Fi gratuite, et d'une salle de bain privative avec douche ou baignoire. Un bureau fonctionnel et un espace de rangement sont également à votre disposition. La décoration moderne et chaleureuse offre une ambiance propice à la détente. Idéale pour les séjours professionnels ou touristiques, cette chambre vous garantit un repos optimal."
+      image="/assets/image/parisluxe.png"
+      imageAlt="Chambre de luxe"
+      link="/reservation"
+      buttonText="Réserver"
+  />
 </template>
 <script setup lang="js">
 const heroPageData = {
