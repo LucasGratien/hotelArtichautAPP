1a51f49fac5082fb5c421fef7f26efcbd6385234
lucas <lgratien@afi-sa.fr>
2025-02-20 09:25:32 +0100

    fetch id

--- pages/roomdetail/[id].vue
+++ pages/roomdetail/[id].vue
@@ -1,11 +1,12 @@
 <template>
   <div class="text-container mx-auto ui-card flex flex-col lg:flex-row items-end lg:justify-between lg:flex md:align-bottom  p-8 rounded-lg w-1/2 mt-4">
   <div class="container mx-auto p-8" v-if="room">
-    <h1 class="text-4xl font-bold mb-4 text-gray-700">{{room.category_name}}</h1>
+    <h1 class="text-4xl font-bold mb-4 text-gray-700 flex justify-center">{{room.category_name}}</h1>
     <img :src="room.image" :alt="room.name" class="rounded-lg w-full max-w-4xl mb-4" />
-    <p class="mb-4 text-gray-700">{{ room.description }}</p>
+    <p class="mb-4 font-bold text-gray-700 flex justify-center">{{ room.description }}</p>
     <div v-for="feature in room.features">
-    <p class="mb-4 text-gray-700">{{feature.name}}</p>
+    <p class="mb-4 font-bold text-gray-700 flex justify-center">{{feature.feature_name}}</p>
+      <p class="mb-4 text-gray-700 flex justify-center">{{feature.description}}</p>
     </div>
     <p class="text-lg flex justify-end font-semibold mt-4 text-gray-700">{{ room.price_in_cent }}€</p>
   </div>
