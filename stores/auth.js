1a876e3eef98511c75095a16620821ff51db67a1
RDelory74 <roro_spike@hotmail.com>
2025-02-27 16:33:42 +0100

    add auth.js store

---
+++ stores/auth.js
@@ -0,0 +1,35 @@
+export const useAuth = defineStore(
+    "user", {
+        state: () => {
+            return {
+                user: '',
+                token : '',
+                isLoggedIn: false,
+            }
+        },
+        getters: {
+            getUser() {
+                return this.user;
+            },
+            getToken() {
+                return this.token;
+            }
+        },
+        actions: {
+            async login(body) {
+                const response = await backend.post("/login", body);
+                if(response.data) {
+                    console.log("login success for ",body.email);
+                    this.user = response.data;
+                    this.token = response.data.token;
+                    this.isLoggedIn = true;
+                } else {
+                    console.log("failed login for ",body.email);
+                    this.user = '';
+                    this.token = '';
+                    this.isLoggedIn = false;
+                }
+            },
+        }
+    }
+);
