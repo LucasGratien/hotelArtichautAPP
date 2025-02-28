4cff574d58d89a4e83536b603e3456ea6425f749
RDelory74 <roro_spike@hotmail.com>
2025-02-28 13:58:37 +0100

    update PageTransition.vue

--- components/ui/PageTransition.vue
+++ components/ui/PageTransition.vue
@@ -5,11 +5,20 @@
         :initial="{ opacity: 0, y: 100, scale: 0.5 }"
         :enter="{ opacity: 1, y: 0, scale: 1.8 }"
         :leave="{ opacity: 0, y: -100, scale: 1.2 }"
-        :transition="{ duration: 1, ease: 'ease-in-out' }"
+        :transition="{
+    duration: 1,
+    ease: 'ease-in-out',
+    opacity: 'ease-out',
+    delay: 0.2
+  }"
         :src="logoSrc"
         alt="L'Artichaut Logo"
         class="logo"
     />
+
+
+
+
   </div>
 </template>
 
@@ -31,3 +40,22 @@ router.afterEach(() => {
   }, 1000);
 });
 </script>
+
+<style scoped>
+.page-transition {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.94);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+}
+
+.logo {
+  width: 350px;
+}
+</style>
