875c916cf2f2ee21daeefdb34ac9decda0227d00
RDelory74 <roro_spike@hotmail.com>
2025-03-24 10:17:31 +0100

    update grammatical comment useApiFetch.js

--- composables/useApiFetch.js
+++ composables/useApiFetch.js
@@ -1,16 +1,16 @@
 import {useAuthStore} from '~/stores/auth';
-import {jwtDecode} from 'jwt-decode';
+
 
 
 export const useApiFetch = (path, options = {}) => {
     const config = useRuntimeConfig();
     const authStore = useAuthStore();
 
-    //Utilisatioin de la config env pour url
+    //Utilisation de la config env pour url
 
     options.baseURL = config.public.apiBaseUrl;
 
-    // Ajouter le token si dispo
+    // Ajouter le token si disco
 
     if (authStore.getToken) {
         options.headers = {
@@ -18,13 +18,13 @@ export const useApiFetch = (path, options = {}) => {
             Authorization: `Bearer ${authStore.getToken}`,
         };
 
-        // Décoder le token et up le store si nécessaire // avec vérification de execution du code coté client
+        // Décoder le token et up le store si nécessaire // avec vérification execution du code coté client
         // if (import.meta.client) {
         //     const decodedToken = jwtDecode(authStore.getToken);
         //     authStore.setUserFromToken(authStore.getToken);
         // }
     }
 
-    //return des data, Attention ça fait geulé (warn) la console, elle préfère fetch
+    //return des data, Attention ça fait râler (warn) la console, elle préfère fetch
     return useFetch(path, options);
 };
