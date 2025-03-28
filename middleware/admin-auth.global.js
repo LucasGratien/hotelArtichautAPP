2285a5eed6c6d057bb9495499f47120cc57d599b
lucas <lucas.gratien@le-campus-numerique.fr>
2025-03-28 11:47:10 +0100

    Récupération des fichiers supprimés

---
+++ middleware/admin-auth.global.js
@@ -0,0 +1,25 @@
+// Middleware avec pour condition la route, la connection du user et le role (+de 2)
+
+export default defineNuxtRouteMiddleware((to, from) => {
+    const authStore = useAuthStore();
+
+    authStore.initializeAuth();
+
+    //pour les routes d'admin
+    if (to.path.startsWith('/admin')) {
+        // isLoggedIn doit être true
+        if (!authStore.isLoggedIn) {
+            console.log('admin auth was not logged in.');
+            alert(`pas connécté !`);
+            //sinon retour à la homepage
+            return navigateTo('/');
+        }
+
+        // le role_id doit être suffisant
+        if (authStore.tokenUser.role_id < 2) {
+            console.log('admin auth was not unable to access.');
+            // Rediriger vers une page accès refusé
+            return navigateTo('/unauthorized');
+        }
+    }
+});
---
+++ pages/unauthorized.vue
@@ -0,0 +1,56 @@
+
+<template>
+  <div  class="unauthorized-container" >
+    <h1>Accès refusé</h1>
+    <p>Vous n'avez pas les autorisations nécessaires pour accéder à cette page.</p>
+    <UButton color="neutral" variant="outline">
+    <NuxtLink to="/">Retour à l'accueil</NuxtLink>
+    </UButton>
+  </div>
+</template>
+
+<style scoped >
+.unauthorized-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+  background-color: var(--background-color);
+  color: var(--black-color);
+}
+
+h1 {
+  color: var(--secondary-color);
+  font-size: 2.5rem;
+}
+
+p {
+  color: var(--champ-color);
+  font-size: 1.2rem;
+  margin-bottom: 20px;
+}
+
+UButton {
+  background-color: var(--primary-color);
+  border: 2px solid var(--secondary-color);
+  color: var(--black-color);
+  padding: 10px 20px;
+  font-size: 1.2rem;
+  transition: all 0.3s ease;
+}
+
+UButton:hover {
+  background-color: var(--secondary-color);
+  color: var(--background-color);
+}
+</style>
+
+<script setup>
+import {useAuthStore} from '~/stores/auth';
+const authStore = useAuthStore();
+onMounted(() => {
+  authStore.initializeAuth();
+});
+</script>
