569379b71f5d646c3377c10e1c7be000d71f609c
RDelory74 <roro_spike@hotmail.com>
2025-03-28 10:07:23 +0100

    Refacto TheFooter.vue (19lignes vs 200)

---
+++ components/ui/NewLettersPod.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <!-- Subscription Section -->
+  <div class="bg-[var(--primary-color)] py-4 rounded text-center md:flex md:flex-row md:flex-nowrap  md:mx-8">
+    <div class="md:w-2/3 flex items-center justify-center">
+      <h2 class="text-[var(--secondary-color)]  md:pt-0 text-2xl md:text-4xl">
+        Stay up to date about our latest offers
+      </h2>
+    </div>
+    <div class="flex flex-col sm:flex-row md:flex-col md:w-1/3 justify-center p-2 items-center gap-4">
+      <input
+          type="email"
+          placeholder="Enter your email ..."
+          class="bg-[var(--secondary-color)] text-[var(--primary-color)] px-10 py-2 rounded-md hover:bg-[#365132] focus:outline-none focus:ring focus:ring-[#4b6447]
+              hover:scale-105 transition-transform duration-300 ease-in-out"/>
+      <button
+          class="bg-[var(--secondary-color)] text-[var(--primary-color)] px-8 py-2 rounded-md hover:bg-[#365132]
+              hover:scale-105 transition-transform duration-300 ease-in-out">
+        Subscribe to the Newsletter
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/ui/TextInfoFooter.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="md:w-1/4">
+    <h3 class="text-[var(--secondary-color)] text-3xl ">L’Artichaut</h3>
+    <p class="text-[var(--black-color)] leading-relaxed  pt-4">
+      665 Route des bains <br/>
+      74250 Menthon <br/>
+      Tél.: +33 454 987 944 <br/>
+      reception@lartichaut.fr
+    </p>
+    <!-- Media Icons -->
+    <UiMediaIconsFooter/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
