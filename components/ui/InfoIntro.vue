2d0452239ffa7e209995086a5800fd330e621088
RDelory74 <roro_spike@hotmail.com>
2025-04-22 12:15:12 +0200

    updated BookingList.vue updated Carousel.vue change ui tabvle in ContentForm.vue add InfoIntro.vue in Equipe.vue add ui style in useradmin.vue add ui style in UserBookingHistory.vue

---
+++ components/ui/InfoIntro.vue
@@ -0,0 +1,19 @@
+<script setup >
+defineProps({
+      title: String,
+      short_description: String,
+      description: String,
+      image: String,
+    }
+)
+</script>
+
+<template>
+  <div class="md:py-12 md:px-48">
+    <h1 class="font-bold text-4xl">L’Équipe : Une Excellence au Service de Votre Séjour</h1>
+    <p class="text-2xl py-4 md:px-10">Notre équipe incarne l’excellence et le raffinement,
+      afin de garantir une expérience inoubliable à chacun de nos invités.
+      Chaque membre est sélectionné avec soin pour ses compétences, son savoir-faire,
+      et surtout son attention aux moindres détails.</p>
+  </div>
+</template>
